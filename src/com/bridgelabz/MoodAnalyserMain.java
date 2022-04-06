1029f4b0fa907ad49b60d14c726e7d0c94443fbb
Amol Ghotale <ghotaleamol@gmail.com>
2022-04-06 13:59:28 +0530

    ADDED: custom exception

--- src/com/bridgelabz/MoodAnalyserMain.java
+++ src/com/bridgelabz/MoodAnalyserMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class MoodAnalyserMain {
     public static void main(String[] args) {
-        MoodAnalyser mood = new MoodAnalyser(null);
+        MoodAnalyser mood = new MoodAnalyser("");
         System.out.println(mood.analyseMood());
     }
 }
