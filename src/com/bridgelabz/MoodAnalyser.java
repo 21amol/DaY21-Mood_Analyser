66e5d804ae22da56ee8583863af8b36fade720e2
Amol Ghotale <ghotaleamol@gmail.com>
2022-04-06 13:15:15 +0530

    ADDED: Null exception and test cases

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-   static String message;
+    static String message;
 
     public MoodAnalyser(String message) {
         this.message = message;
@@ -10,11 +10,16 @@ public class MoodAnalyser {
     public static String analyseMood() {
         String messageOne = "I am in Sad Mood";
         String messageTwo = "I am in any Mood";
-        if (message.equalsIgnoreCase(messageOne))
-            return "SAD";
-        else if (message.equalsIgnoreCase(messageTwo))
-            return "HAPPY";
-        else
-            return "No Match";
+        try {
+            if (message.equalsIgnoreCase(messageOne))
+                return "SAD";
+            else if (message.equalsIgnoreCase(messageTwo))
+                return "HAPPY";
+            else
+                return "No Match";
+
+        } catch (NullPointerException e) {
+            return"HAPPY(null)";
+        }
     }
 }
--- src/com/bridgelabz/MoodAnalyserMain.java
+++ src/com/bridgelabz/MoodAnalyserMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class MoodAnalyserMain {
     public static void main(String[] args) {
-        MoodAnalyser mood = new MoodAnalyser("I am in Sad Mood");
+        MoodAnalyser mood = new MoodAnalyser(null);
         System.out.println(mood.analyseMood());
     }
 }
